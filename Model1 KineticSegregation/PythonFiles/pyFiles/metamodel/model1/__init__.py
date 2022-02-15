462fd2671443f45deb1fc80e6402cde634bcecfb
Yair Neve-Oz <yairneveoz@cs.huji.ac.il>
2022-02-15 17:35:51 +0200

    Update Model1 KineticSegregation

--- Model1 KineticSegregation/PythonFiles/pyFiles/metamodel/model1/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb  1 13:05:23 2022
-
-@author: yairn
-"""
-
--- Model1 KineticSegregation/PythonFiles/pyFiles/metamodel/model2/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb  7 16:28:13 2022
-
-@author: yairn
-"""
-
